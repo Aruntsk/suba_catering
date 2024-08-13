aa320d3b9f9244e030ac18ef3ba53f0206881577
aruntsk <arunsakthivel279@gmail.com>
2024-08-13 20:08:49 +0530

    Menu Module Updated

---
+++ suba_catering/Admin/Menus.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/AdminMaster.Master" AutoEventWireup="true" CodeBehind="Menus.aspx.cs" Inherits="suba_catering.Admin.Menus" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <div class="container p-md-4 p-sm-4">
+    <div>
+        <asp:Label ID="lblMsg" runat="server"></asp:Label>
+    </div>
+    <h3 class="text-center">Manage Menus</h3>
+    <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-5">
+        <div class="col-md-6">
+            <label for="ddlCategory">Category</label>
+            <asp:DropDownList ID="ddlCategory" runat="server" CssClass="form-control" AutoPostBack="True" OnSelectedIndexChanged="ddlCategory_SelectedIndexChanged"></asp:DropDownList>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Category is Required." ControlToValidate="ddlCategory" Display="Dynamic" ForeColor="Red" InitialValue="Select Category" SetFocusOnError="True"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-2">
+        <div class="col-md-6">
+            <label for="ddlSubCategory">Sub Category</label>
+            <asp:DropDownList ID="ddlSubCategory" runat="server" CssClass="form-control"></asp:DropDownList>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="Sub Category is Required." ControlToValidate="ddlSubCategory" Display="Dynamic" ForeColor="Red" InitialValue="0" SetFocusOnError="True"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-2">
+        <div class="col-md-6">
+            <label for="txtTitle">Title</label>
+            <asp:TextBox ID="txtTitle" runat="server" CssClass="form-control" placeholder="Enter Menu Title" required="true"></asp:TextBox>
+        </div>
+    </div>
+    <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-2">
+        <div class="col-md-6">
+            <label for="txtDescription">Description</label>
+            <asp:TextBox ID="txtDescription" runat="server" CssClass="form-control" placeholder="Enter Description" required="true"></asp:TextBox>
+        </div>
+    </div>
+    <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-2">
+        <div class="col-md-3 col-md-offset-2 mb-3">
+            <asp:Button ID="btnAdd" Text="Submit" runat="server" CssClass="btn btn-primary" OnClick="btnAdd_Click" />
+        </div>
+    </div>
+
+    <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-2">
+        <div class="col-md-12">
+            <asp:GridView ID="gridview1" runat="server" CssClass="table table-hover table-bordered" DataKeyNames="id" EmptyDataText="No record to display" AutoGenerateColumns="False" OnRowEditing="gridview1_RowEditing" OnPageIndexChanging="gridview1_PageIndexChanging" OnRowCancelingEdit="gridview1_RowCancelingEdit" OnRowUpdating="gridview1_RowUpdating" AllowPaging="True" PageSize="5" OnRowDeleting="gridview1_RowDeleting">
+                <Columns>
+                    <asp:BoundField DataField="Sr.No" HeaderText="Sr.No" ReadOnly="True">
+                        <ItemStyle HorizontalAlign="Center"></ItemStyle>
+                    </asp:BoundField>
+                     <asp:BoundField DataField="category" HeaderText="Category" ReadOnly="True">
+                         <ItemStyle HorizontalAlign="Center"></ItemStyle>
+                     </asp:BoundField>
+                    <asp:BoundField DataField="sub_category" HeaderText="Sub Category" ReadOnly="True">
+                        <ItemStyle HorizontalAlign="Center"></ItemStyle>
+                    </asp:BoundField>
+                    <asp:TemplateField HeaderText="Title">
+                        <EditItemTemplate>
+                            <asp:TextBox ID="TxtTitle" runat="server" Text='<%# Eval("title") %>' CssClass="form-control"></asp:TextBox>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Label ID="LblTitle" runat="server" Text='<%# Eval("title") %>'></asp:Label>
+                        </ItemTemplate>
+                        <ItemStyle HorizontalAlign="Center" />
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Description">
+                        <EditItemTemplate>
+                            <asp:TextBox ID="TxtDescription" runat="server" Text='<%# Eval("description") %>' CssClass="form-control"></asp:TextBox>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Label ID="LblDescription" runat="server" Text='<%# Eval("description") %>'></asp:Label>
+                        </ItemTemplate>
+                        <ItemStyle HorizontalAlign="Center" />
+                    </asp:TemplateField>
+                    <asp:CommandField CausesValidation="False" HeaderText="Operation" ShowEditButton="True" ShowDeleteButton="True" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </div>
+</div>
+</asp:Content>
---
+++ suba_catering/Admin/Menus.aspx.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static suba_catering.Models.CommonFn;
+
+namespace suba_catering.Admin
+{
+    public partial class Menus : System.Web.UI.Page
+    {
+        CommonFnx fn = new CommonFnx();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetCategories();
+                GetMenus();
+            }
+        }
+
+        private void GetCategories()
+        {
+            DataTable dt = fn.fetch("Select * from categories");
+            ddlCategory.DataSource = dt;
+            ddlCategory.DataTextField = "category";
+            ddlCategory.DataValueField = "id";
+            ddlCategory.DataBind();
+            ddlCategory.Items.Insert(0, "Select Category");
+        }
+
+        private void GetSubCategories()
+        {
+            // Ensure the selected category ID is valid
+            if (int.TryParse(ddlCategory.SelectedItem.Value, out int categoryId) && categoryId > 0)
+            {
+                DataTable dt = fn.fetch($"SELECT * FROM sub_categories WHERE parent_id = {categoryId}");
+                ddlSubCategory.DataSource = dt;
+                ddlSubCategory.DataTextField = "sub_category";
+                ddlSubCategory.DataValueField = "id";
+                ddlSubCategory.DataBind();
+                ddlSubCategory.Items.Insert(0, new ListItem("Select Sub Category", "0"));
+            }
+            else
+            {
+                // Clear sub-category dropdown if no valid category is selected
+                ddlSubCategory.Items.Clear();
+                ddlSubCategory.Items.Insert(0, new ListItem("Select Sub Category", "0"));
+            }
+        }
+
+        private void GetMenus()
+        {
+            DataTable dt = fn.fetch("Select ROW_NUMBER() OVER (Order by m.id) as [Sr.No], m.id, c.category, sc.sub_category, m.title, m.description from menus m inner join categories c on c.id = m.category_id inner join sub_categories sc on m.sub_category_id = sc.id");
+            gridview1.DataSource = dt;
+            gridview1.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sub_category = ddlSubCategory.SelectedItem.Text;
+                DataTable dt = fn.fetch("Select * from menus where sub_category_id = '" + ddlSubCategory.SelectedItem.Value + "' and title = '" + txtTitle.Text.Trim() + "'");
+                if (dt.Rows.Count == 0)
+                {
+                    string query = "Insert into menus (category_id, sub_category_id, title, description) values ('" + ddlCategory.SelectedItem.Value + "', '" + ddlSubCategory.SelectedItem.Value + "', '" + txtTitle.Text.Trim() + "','" + txtDescription.Text.Trim() + "')";
+                    fn.Query(query);
+                    lblMsg.Text = "Inserted Successfully";
+                    lblMsg.CssClass = "alert alert-success";
+                    ddlCategory.SelectedIndex = 0;
+                    txtTitle.Text = string.Empty;
+                    txtDescription.Text = string.Empty;
+                    GetMenus();
+                }
+                else
+                {
+                    lblMsg.Text = "Title Already Exits for <b> '" + sub_category + "' </b>";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        protected void gridview1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gridview1.PageIndex = e.NewPageIndex;
+            GetMenus();
+        }
+
+        protected void gridview1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gridview1.EditIndex = -1;
+            GetMenus();
+        }
+
+        protected void gridview1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gridview1.EditIndex = e.NewEditIndex;
+            GetMenus();
+        }
+
+        protected void gridview1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = gridview1.Rows[e.RowIndex];
+                int mId = Convert.ToInt32(gridview1.DataKeys[e.RowIndex].Values[0]);
+                string title = (row.FindControl("TxtTitle") as TextBox).Text;
+                string description = (row.FindControl("TxtDescription") as TextBox).Text;
+                string query = "Update menus set title= '" + title + "', description = '" + description + "' where id = '" + mId + "'";
+                fn.Query(query);
+                lblMsg.Text = "Updated Successfully";
+                lblMsg.CssClass = "alert alert-success";
+                gridview1.EditIndex = -1;
+                GetMenus();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        protected void gridview1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                int mId = Convert.ToInt32(gridview1.DataKeys[e.RowIndex].Values[0]);
+                fn.Query("Delete from menus where id = '" + mId + "'");
+                lblMsg.Text = "Deleted Successfully";
+                lblMsg.CssClass = "alert alert-success";
+                gridview1.EditIndex = -1;
+                GetMenus();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        protected void ddlCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetSubCategories();
+        }
+    }
+}
