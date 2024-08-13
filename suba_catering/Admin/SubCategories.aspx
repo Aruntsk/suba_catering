925228535f92c3772338dcbd017212e330fdfe7b
aruntsk <arunsakthivel279@gmail.com>
2024-08-13 20:08:26 +0530

    Sub Categories Module Updated

---
+++ suba_catering/Admin/SubCategories.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/AdminMaster.Master" AutoEventWireup="true" CodeBehind="SubCategories.aspx.cs" Inherits="suba_catering.Admin.SubCategories" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container p-md-4 p-sm-4">
+    <div>
+        <asp:Label ID="lblMsg" runat="server"></asp:Label>
+    </div>
+    <h3 class="text-center">Manage Sub Categories</h3>
+    <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-5">
+        <div class="col-md-6">
+            <label for="ddlCategory">Category</label>
+            <asp:DropDownList ID="ddlCategory" runat="server" CssClass="form-control"></asp:DropDownList>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Category is Required." ControlToValidate="ddlCategory" Display="Dynamic" ForeColor="Red" InitialValue="Select Category" SetFocusOnError="True"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-2">
+        <div class="col-md-6">
+            <label for="txtCategory">Sub Category Name</label>
+            <asp:TextBox ID="txtSubCategory" runat="server" CssClass="form-control" placeholder="Enter Sub Category Name" required="true"></asp:TextBox>
+        </div>
+    </div>
+    <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-2">
+        <div class="col-md-6">
+            <label for="txtDescription">Description</label>
+            <asp:TextBox ID="txtDescription" runat="server" CssClass="form-control" placeholder="Enter Sub Category Description" required="true"></asp:TextBox>
+        </div>
+    </div>
+    <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-2">
+        <div class="col-md-3 col-md-offset-2 mb-3">
+            <asp:Button ID="btnAdd" Text="Submit" runat="server" CssClass="btn btn-primary" OnClick="btnAdd_Click" />
+        </div>
+    </div>
+
+    <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-2">
+        <div class="col-md-12">
+            <asp:GridView ID="gridview1" runat="server" CssClass="table table-hover table-bordered" DataKeyNames="id" EmptyDataText="No record to display" AutoGenerateColumns="False" OnRowEditing="gridview1_RowEditing" OnPageIndexChanging="gridview1_PageIndexChanging" OnRowCancelingEdit="gridview1_RowCancelingEdit" OnRowUpdating="gridview1_RowUpdating" AllowPaging="True" PageSize="5" OnRowDeleting="gridview1_RowDeleting">
+                <Columns>
+                    <asp:BoundField DataField="Sr.No" HeaderText="Sr.No" ReadOnly="True">
+                        <ItemStyle HorizontalAlign="Center"></ItemStyle>
+                    </asp:BoundField>
+                     <asp:BoundField DataField="category" HeaderText="Category" ReadOnly="True">
+                         <ItemStyle HorizontalAlign="Center"></ItemStyle>
+                     </asp:BoundField>
+                    <asp:TemplateField HeaderText="Sub Category Name">
+                        <EditItemTemplate>
+                            <asp:TextBox ID="TxtSubCategoryName" runat="server" Text='<%# Eval("sub_category") %>' CssClass="form-control"></asp:TextBox>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Label ID="LblCategoryName" runat="server" Text='<%# Eval("sub_category") %>'></asp:Label>
+                        </ItemTemplate>
+                        <ItemStyle HorizontalAlign="Center" />
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Description">
+                        <EditItemTemplate>
+                            <asp:TextBox ID="TxtDescription" runat="server" Text='<%# Eval("description") %>' CssClass="form-control"></asp:TextBox>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Label ID="LblDescription" runat="server" Text='<%# Eval("description") %>'></asp:Label>
+                        </ItemTemplate>
+                        <ItemStyle HorizontalAlign="Center" />
+                    </asp:TemplateField>
+                    <asp:CommandField CausesValidation="False" HeaderText="Operation" ShowEditButton="True" ShowDeleteButton="True" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </div>
+</div>
+</asp:Content>
---
+++ suba_catering/Admin/SubCategories.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static suba_catering.Models.CommonFn;
+
+namespace suba_catering.Admin
+{
+    public partial class SubCategories : System.Web.UI.Page
+    {
+        CommonFnx fn = new CommonFnx();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetCategories();
+                GetSubCategories();
+            }
+        }
+
+        private void GetCategories()
+        {
+            DataTable dt = fn.fetch("Select * from categories");
+            ddlCategory.DataSource = dt;
+            ddlCategory.DataTextField = "category";
+            ddlCategory.DataValueField = "id";
+            ddlCategory.DataBind();
+            ddlCategory.Items.Insert(0, "Select Category");
+        }
+
+        private void GetSubCategories()
+        {
+            DataTable dt = fn.fetch("Select ROW_NUMBER() OVER (Order by s.id) as [Sr.No], s.id, s.parent_id, c.category, s.sub_category, s.description from sub_categories s inner join categories c on c.id = s.parent_id");
+            gridview1.DataSource = dt;
+            gridview1.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string category = ddlCategory.SelectedItem.Text;
+                DataTable dt = fn.fetch("Select * from sub_categories where parent_id = '" + ddlCategory.SelectedItem.Value + "' and sub_category = '" + txtSubCategory.Text.Trim() + "'");
+                if (dt.Rows.Count == 0)
+                {                    
+                    string query = "Insert into sub_categories (parent_id, sub_category, description) values ('" + ddlCategory.SelectedItem.Value + "','" + txtSubCategory.Text.Trim() + "','" + txtDescription.Text.Trim() + "')";
+                    fn.Query(query);
+                    lblMsg.Text = "Inserted Successfully";
+                    lblMsg.CssClass = "alert alert-success";
+                    ddlCategory.SelectedIndex = 0;
+                    txtSubCategory.Text = string.Empty;
+                    txtDescription.Text = string.Empty;
+                    GetSubCategories();
+                }
+                else
+                {
+                    lblMsg.Text = "Sub Category Name Already Exits for Category <b> '"+ category + "' </b>";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        protected void gridview1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gridview1.PageIndex = e.NewPageIndex;
+            GetSubCategories();
+        }
+
+        protected void gridview1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gridview1.EditIndex = -1;
+            GetSubCategories();
+        }
+
+        protected void gridview1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gridview1.EditIndex = e.NewEditIndex;
+            GetSubCategories();
+        }
+
+        protected void gridview1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = gridview1.Rows[e.RowIndex];
+                int cId = Convert.ToInt32(gridview1.DataKeys[e.RowIndex].Values[0]);
+                string sub_category = (row.FindControl("TxtSubCategoryName") as TextBox).Text;
+                string description = (row.FindControl("TxtDescription") as TextBox).Text;
+                string query = "Update sub_categories set sub_category= '" + sub_category + "', description = '" + description + "' where id = '" + cId + "'";
+                fn.Query(query);
+                lblMsg.Text = "Updated Successfully";
+                lblMsg.CssClass = "alert alert-success";
+                gridview1.EditIndex = -1;
+                GetSubCategories();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        protected void gridview1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                int cId = Convert.ToInt32(gridview1.DataKeys[e.RowIndex].Values[0]);
+                fn.Query("Delete from sub_categories where id = '" + cId + "'");
+                lblMsg.Text = "Deleted Successfully";
+                lblMsg.CssClass = "alert alert-success";
+                gridview1.EditIndex = -1;
+                GetSubCategories();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+    }
+}
