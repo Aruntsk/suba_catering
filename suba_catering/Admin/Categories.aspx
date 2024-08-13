c58e661a670cadeaf185135728996145737906a0
aruntsk <arunsakthivel279@gmail.com>
2024-08-13 20:07:54 +0530

    Categories Module Updated

---
+++ suba_catering/Admin/Categories.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/AdminMaster.Master" AutoEventWireup="true" CodeBehind="Categories.aspx.cs" Inherits="suba_catering.Admin.Categories" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container p-md-4 p-sm-4">
+        <div>
+            <asp:Label ID="lblMsg" runat="server"></asp:Label>
+        </div>
+        <h3 class="text-center">Manage Categories</h3>
+        <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-5">
+            <div class="col-md-6">
+                <label for="txtCategory">Category Name</label>
+                <asp:TextBox ID="txtCategory" runat="server" CssClass="form-control" placeholder="Enter Category Name" required="true"></asp:TextBox>
+            </div>
+        </div>
+        <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-2">
+            <div class="col-md-6">
+                <label for="txtDescription">Description</label>
+                <asp:TextBox ID="txtDescription" runat="server" CssClass="form-control" placeholder="Enter Category Description"></asp:TextBox>
+            </div>
+        </div>
+        <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-2">
+            <div class="col-md-3 col-md-offset-2 mb-3">
+                <asp:Button ID="btnAdd" Text="Submit" runat="server" CssClass="btn btn-primary" OnClick="btnAdd_Click" />
+            </div>
+        </div>
+
+        <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-2">
+            <div class="col-md-12">
+                <asp:GridView ID="gridview1" runat="server" CssClass="table table-hover table-bordered" DataKeyNames="id" EmptyDataText="No record to display" AutoGenerateColumns="False" OnRowEditing="gridview1_RowEditing" OnPageIndexChanging="gridview1_PageIndexChanging" OnRowCancelingEdit="gridview1_RowCancelingEdit" OnRowUpdating="gridview1_RowUpdating" AllowPaging="True" PageSize="5" OnRowDeleting="gridview1_RowDeleting">
+                    <Columns>
+                        <asp:BoundField DataField="Sr.No" HeaderText="Sr.No" ReadOnly="True">
+                            <ItemStyle HorizontalAlign="Center"></ItemStyle>
+                        </asp:BoundField>
+                        <asp:TemplateField HeaderText="Name">
+                            <EditItemTemplate>
+                                <asp:TextBox ID="TxtCategoryName" runat="server" Text='<%# Eval("category") %>' CssClass="form-control"></asp:TextBox>
+                            </EditItemTemplate>
+                            <ItemTemplate>
+                                <asp:Label ID="LblCategoryName" runat="server" Text='<%# Eval("category") %>'></asp:Label>
+                            </ItemTemplate>
+                            <ItemStyle HorizontalAlign="Center" />
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Description">
+                            <EditItemTemplate>
+                                <asp:TextBox ID="TxtDescription" runat="server" Text='<%# Eval("description") %>' CssClass="form-control"></asp:TextBox>
+                            </EditItemTemplate>
+                            <ItemTemplate>
+                                <asp:Label ID="LblDescription" runat="server" Text='<%# Eval("description") %>'></asp:Label>
+                            </ItemTemplate>
+                            <ItemStyle HorizontalAlign="Center" />
+                        </asp:TemplateField>
+                        <asp:CommandField CausesValidation="False" HeaderText="Operation" ShowEditButton="True" ShowDeleteButton="True" />
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ suba_catering/Admin/Categories.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static suba_catering.Models.CommonFn;
+
+namespace suba_catering.Admin
+{
+    public partial class Categories : System.Web.UI.Page
+    {
+        CommonFnx fn = new CommonFnx();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetCategories();
+            }
+        }
+        private void GetCategories()
+        {
+            DataTable dt = fn.fetch("Select Row_NUMBER() over(Order by (Select 1)) as [Sr.No], id, category, description, date_created from categories");
+            gridview1.DataSource = dt;
+            gridview1.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = fn.fetch("Select * from categories where category = '" + txtCategory.Text.Trim() + "' ");
+                if (dt.Rows.Count == 0)
+                {                
+                    string query = "Insert into categories (category, description) values ('" + txtCategory.Text.Trim() + "','" + txtDescription.Text.Trim() + "')";
+                    fn.Query(query);
+                    lblMsg.Text = "Inserted Successfully";
+                    lblMsg.CssClass = "alert alert-success";
+                    txtCategory.Text = string.Empty;
+                    txtDescription.Text = string.Empty;
+                    GetCategories();
+                }
+                else
+                {
+                    lblMsg.Text = "Category Name Already Exits";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        protected void gridview1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gridview1.PageIndex = e.NewPageIndex;
+            GetCategories();
+        }
+
+        protected void gridview1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gridview1.EditIndex = -1;
+            GetCategories();
+        }
+
+        protected void gridview1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gridview1.EditIndex = e.NewEditIndex;
+            GetCategories();
+        }
+
+        protected void gridview1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = gridview1.Rows[e.RowIndex];
+                int cId = Convert.ToInt32(gridview1.DataKeys[e.RowIndex].Values[0]);
+                string category = (row.FindControl("TxtCategoryName") as TextBox).Text;
+                string description = (row.FindControl("TxtDescription") as TextBox).Text;
+                string query = "Update categories set category= '" + category + "', description = '" + description + "' where id = '" + cId + "'";
+                fn.Query(query);
+                lblMsg.Text = "Updated Successfully";
+                lblMsg.CssClass = "alert alert-success";
+                gridview1.EditIndex = -1;
+                GetCategories();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        protected void gridview1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                int cId = Convert.ToInt32(gridview1.DataKeys[e.RowIndex].Values[0]);
+                fn.Query("Delete from categories where id = '" + cId + "'");
+                lblMsg.Text = "Deleted Successfully";
+                lblMsg.CssClass = "alert alert-success";
+                gridview1.EditIndex = -1;
+                GetCategories();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+    }
+}
